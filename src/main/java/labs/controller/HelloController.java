410e56a6775b25a968219e1317162032a568fd45
Andrey Kurganov <andrey.kurganov@bcgdv.com>
2017-04-10 16:58:14 -0700

    Rest Controllers Created

---
+++ src/main/java/labs/controller/HelloController.java
@@ -0,0 +1,15 @@
+package labs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String helloPage() {
+
+		return "hello-page";
+	}
+
+}
---
+++ src/main/java/labs/controller/MainController.java
@@ -0,0 +1,17 @@
+package labs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String mainPage() {
+
+		return "... comming soon";
+	}
+
+}
