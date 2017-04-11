9a854890d2aa0b4217bb173b42cf8fcf4bf43b9e
Andrey Kurganov <andrey.kurganov@bcgdv.com>
2017-04-11 14:33:28 -0700

    finished with lab 1

--- src/main/java/labs/MySpringLabsApplication.java
+++ src/main/java/labs/MySpringLabsApplication.java
@@ -1,12 +1,43 @@
 package labs;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import labs.controller.Player;
+import labs.repository.TeamRepository;
 
 @SpringBootApplication
 public class MySpringLabsApplication {
 
+	@Autowired
+	public TeamRepository teamRepository;
+
+	@PostConstruct
+	public void init() {
+		List<Team> list = new ArrayList<>();
+
+		Set<Player> set = new HashSet<>();
+		set.add(new Player("Big Easy", "Showman"));
+		set.add(new Player("Buckets", "Guard"));
+		set.add(new Player("Dizzy", "Guard"));
+
+		list.add(new Team("Harlem", "Globetrotters", set));
+		list.add(new Team("Washington", "Generals", null));
+
+		teamRepository.save(list);
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(MySpringLabsApplication.class, args);
 	}
+
 }
